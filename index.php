e59d960d810ec8512a8e8dfe5b3a58ca9f40892d
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2023-11-15 17:18:04 +0700

    Add Assets

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HerbalPluss</title>
+
+    <!-- Bootstrap 5 CSS -->
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css" />
+</head>
+<body>
+    <!-- Bootstrap 5 JS -->
+    <script src="./bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
