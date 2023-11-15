5e39cf5d498a8a37c1748ad523e22b8bcfba1302
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2023-11-15 17:25:06 +0700

    Initialize Project

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HerbalPluss</title>
+    <title>Document</title>
 
     <!-- Bootstrap 5 CSS -->
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css" />
