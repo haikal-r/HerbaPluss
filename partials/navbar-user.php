a9afe5597d9e1b137ac376abe07e51e525676732
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2023-11-27 21:43:46 +0700

    add navbar-user

---
+++ partials/navbar-user.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg bg-navigation-primary">
+  <div class="container-fluid px-5">
+    <a class="navbar-brand ms-3" href="#">
+    <img src="../assets/HerbaPluss.svg" alt="" width="200" height="50">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-flex justify-content-center d-none d-sm-block" id="navbarSupportedContent">
+      <form class="d-flex w-50" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+    <a href="#" class="text-white navbar-hover">
+        <i class="fas fa-user-circle fa-xl me-2 "></i>
+    </a>
+    <a href="#" class="text-white navbar-hover ms-3">
+        <i class="fa-solid fa-cart-shopping fa-xl "></i>
+    </a>
+  </div>
+</nav>
---
+++ user/home-page.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="../vendor/fontawesome/fontawesome-free-6.4.2-web/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/main.css" />
+    <title>Document</title>
+</head>
+<body>
+    <?php require"../partials/navbar-user.php" ?>
+   
+</body>
+</html>
