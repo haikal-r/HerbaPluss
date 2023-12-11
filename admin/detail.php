26e97df3e27abd41c345ef5c3ccfff36375831f7
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2023-12-11 20:37:31 +0700

    feat: add slicing design admin dashboard

--- admin/detail.php
+++ admin/detail.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
+require '../config/index.php';
 if (!isset($_SESSION['username'])) {
     header('location: ../login.php ');
     exit;
 }
 
 $user = $_SESSION['username'];
+$hasil = isset($_POST['hasil']) ? (int)$_POST['hasil'] : 2
 ?>
 
 
@@ -32,18 +34,17 @@ $user = $_SESSION['username'];
     <section>
         <div class="container bg-white d-flex my-4 p-3">
             <?php
-            require '../config/index.php';
 
             $id = $_GET['id'];
             $queryProduk = mysqli_query($conn, "SELECT * FROM product WHERE id_product='$id'");
             $produk = mysqli_fetch_array($queryProduk);
             ?>
-            <div class="bg-body-secondary w-100 p-2 d-flex justify-content-center align-items-center me-4">
+            <div class="bg-body-tertiary w-100 p-2 d-flex justify-content-center align-items-center me-4">
                 <img src="../upload/image/<?php echo $produk['gambar'] ?>" alt="" class="w-100">
             </div>
             <div class="d-flex flex-column mt-3 gap-2">
                 <h1 class="fw-bold "><?php echo $produk['nama_barang'] ?></h1>
-                <div class="bg-body-secondary p-4 ">
+                <div class="bg-body-tertiary p-4 shadow-sm">
                     <h1 class="text-success fw-medium">
                         <?php echo $produk['harga'] ?>
                     </h1>
@@ -51,10 +52,6 @@ $user = $_SESSION['username'];
                 <p class="mt-2">
                     <?php echo $produk['deskripsi'] ?>
                 </p>
-                <div class="mt-4 d-flex gap-2">
-                    <button type="button" name="keranjang" class="btn btn-outline-success rounded-0 py-3 px-4">Masukan Keranjang</button>
-                    <button type="button" name="beli" class="btn btn-success rounded-0 py-3 px-5">Beli Sekarang</button>
-                </div>
             </div>
         </div>
     </section>
--- admin/view/dashboard.php
+++ admin/view/dashboard.php
@@ -4,6 +4,7 @@ require('../../partials/session-admin.php');
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,24 +13,60 @@ require('../../partials/session-admin.php');
     <link rel="stylesheet" href="../../assets/css/main.css" />
     <title>Document</title>
 </head>
+
 <body>
-    <?php require"../../partials/navbar-dashboard-admin.php" ?>
+    <?php require "../../partials/navbar-dashboard-admin.php" ?>
     <div class="container-fluid">
-    <div class="row">
-        <div class="col-md-2 bg-navigation-primary pr-3 pt-4">
-            <?php require"../../partials/sidebar-dashboard-admin.php" ?>
+        <div class="row">
+            <div class="col-md-2 bg-navigation-primary pr-3 pt-4">
+                <?php require "../../partials/sidebar-dashboard-admin.php" ?>
+            </div>
+            <div class="col-md-10 p-4">
+                <h3><i class="fas fa-tv me-3"></i>Dashboard</h3>
+                <hr>
+                <!-- main content -->
+                <div class="d-flex justify-content-center gap-4">
+                    <a href="profil.php" class="text-decoration-none">
+                    <div class="card text-bg-primary mb-3 py-2" style="min-width: 22rem;">
+                        <div class="card-body d-flex justify-content-start align-items-center fs-4">
+                            <i class="fas fa-user-circle fa-xl me-3"></i>Profile
+                        </div>
+                        <div class="card-footer d-flex justify-content-between align-items-center">
+                            <p class="card-text my-auto">view detail</p>
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
+                    </a>
+                    <a href="produk.php" class="text-decoration-none">
+                    <div class="card text-bg-warning text-white mb-3 py-2" style="min-width: 22rem;">
+                        <div class="card-body d-flex justify-content-start align-items-center fs-4">
+                            <i class="fa-solid fa-box-open fa-lg me-3"></i>Data Produk
+                        </div>
+                        <div class="card-footer d-flex justify-content-between align-items-center">
+                            <p class="card-text my-auto">view detail</p>
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
+                    </a>
+                    <a href="konsumen.php" class="text-decoration-none">
+                    <div class="card text-bg-danger mb-3 py-2" style="min-width: 22rem;">
+                        <div class="card-body d-flex justify-content-start align-items-center fs-4">
+                            <i class="fa-solid fa-users fa-lg me-3"></i>Data Konsumen
+                        </div>
+                        <div class="card-footer d-flex justify-content-between align-items-center">
+                            <p class="card-text my-auto">view detail</p>
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+
+
+
+                <!-- end content content -->
+            </div>
         </div>
-        <div class="col-md-10 p-4">
-            <h3><i class="fas fa-tv me-3"></i>Dashboard</h3>
-            <hr>
-            <!-- main content -->
-            
-
-
-
-            <!-- end content content -->
-        </div>
-    </div>
     </div>
 </body>
+
 </html>
